3548d25396259d84d103ca86f52b18529e811a50
Travis Vela <jtravisvela@gmail.com>
2019-03-21 10:48:44 -0500

    add exercise 1.b.3 for loop refactor

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,7 +1,7 @@
 
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        for (int i = 100; i >= -10; i -= 5) {
+        for (long i = 2; i <= 1000000; i *= i) {
             System.out.println(i);
         }
     }
