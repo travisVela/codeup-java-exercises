5a98dc94b999fbbc0c35c299d24e3673986c18c0
Travis Vela <jtravisvela@gmail.com>
2019-03-21 14:40:55 -0500

    add exercise 4

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -4,30 +4,28 @@ public class ControlFlowExercises {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter an Integer. ");
-        int num = scanner.nextInt();
+        while (true) {
+            System.out.print("Enter a numerical grade. ");
+            int grade = scanner.nextInt();
 
-        String n = "number";
-        String sq = "squared";
-        String cube = "cubed";
-        int nLength = n.length();
-        int sqLength = sq.length();
-        int cubeLength = cube.length();
+            if (grade >= 88) {
+                System.out.print("The letter grade equivalent is A");
+            } else if (grade < 88 && grade >= 80) {
+                System.out.print("The letter grade equivalent is B");
+            } else if (grade < 80 && grade >= 67) {
+                System.out.print("The letter grade equivalent is C");
+            } else if (grade < 67 && grade >= 60) {
+                System.out.print("The letter grade equivalent is D");
+            } else if (grade < 60) {
+                System.out.print("The letter grade equivalent is F");
+            }
+            System.out.println();
+            scanner.nextLine();
+            System.out.println("Would you like to continue? [y/n]");
 
-        String dash = "-";
-        System.out.println();
-        System.out.println("Here is your table: ");
-        System.out.println();
-        System.out.println();
-        System.out.printf("%-7s| %-7s | %-7s%n", n, sq, cube);
-        System.out.printf("------ | ------- | -----%n");
-
-
-        for (long i = 1; i <= num; i++) {
-            long s = i * i;
-            long c = i * i * i;
-            System.out.printf("%-7d| %-7d | %-7d%n", i, s, c);
-
+            if (scanner.nextLine().equals("n")) {
+                break;
+            }
         }
     }
 }
