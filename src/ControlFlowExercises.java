4b9904f4c0a7f283bbaa7beaabe721de6f561c71
Travis Vela <jtravisvela@gmail.com>
2019-03-21 10:02:54 -0500

    add exersice 1.b.1

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,11 +1,11 @@
 
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 5;
+        int i = 0;
 
-        while (i <= 15) {
-            System.out.print(i + " ");
-            i++;
-        }
+        do {
+            System.out.println(i);
+            i += 2;
+        } while (i <= 100);
     }
 }
