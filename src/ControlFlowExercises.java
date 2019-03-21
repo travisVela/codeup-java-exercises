344e3565fc9115ada38fec8e7ed91e7a0f6b510a
Travis Vela <jtravisvela@gmail.com>
2019-03-21 14:03:41 -0500

    add exercise 3

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,16 +1,33 @@
+import java.util.Scanner;
 
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        for (int i = 1; i <= 100; i++) {
-            if (i % 15 == 0) {
-                System.out.println("FizzBuzz");
-            } else if (i % 5 == 0) {
-                System.out.println("Buzz");
-            } else if (i % 3 == 0) {
-                System.out.println("Fizz");
-            } else {
-                System.out.println(i);
-            }
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter an Integer. ");
+        int num = scanner.nextInt();
+
+        String n = "number";
+        String sq = "squared";
+        String cube = "cubed";
+        int nLength = n.length();
+        int sqLength = sq.length();
+        int cubeLength = cube.length();
+
+        String dash = "-";
+        System.out.println();
+        System.out.println("Here is your table: ");
+        System.out.println();
+        System.out.println();
+        System.out.printf("%-7s| %-7s | %-7s%n", n, sq, cube);
+        System.out.printf("------ | ------- | -----%n");
+
+
+        for (long i = 1; i <= num; i++) {
+            long s = i * i;
+            long c = i * i * i;
+            System.out.printf("%-7d| %-7d | %-7d%n", i, s, c);
+
         }
     }
 }
