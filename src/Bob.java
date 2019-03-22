794495f5ee7fcf3e712434ebb7f6b80c7563922a
Travis Vela <jtravisvela@gmail.com>
2019-03-22 11:03:02 -0500

    add a count to loop to exit at certain number of questions

--- src/Bob.java
+++ src/Bob.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
-import java.awt.event.KeyEvent;
+//import java.awt.event.KeyEvent
 public class Bob {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Hello, meet Bob. He's all ears\ntype 'end' to leave the conversation.");
-
+        System.out.println("Hello, meet Bob. He's all ears.\n(type 'end' to leave the conversation.)");
+        int count = 1;
         while (true) {
             System.out.println("...");
             System.out.println();
@@ -21,15 +21,23 @@ public class Bob {
                 message = "Whoa, chill out!";
                 System.out.println(message);
                 System.out.println();
+            } else if (count == 10) {
+                System.out.println("*  Bob stormed off\n*  Give him space\n*  He'll come around.");
+                break;
+            } else if (count == 5) {
+                System.out.println("*  Bob is getting impatient\n*  tread lightly.");
             } else if (message.equals("end")) {
                 break;
-            } else if (message.equals("") || message.indexOf("?") == -1 || message.indexOf("!") == -1) {
-                message = "Whatever.";
+            } else if (message.equals("")) {
+                message = "Fine. Be that way!";
                 System.out.println(message);
+                System.out.println();
+            } else if (message.indexOf("?") == -1 || message.indexOf("!") == -1) {
+                System.out.println("Whatever.");
                 System.out.println();
             }
 
-
+            count += 1;
 
         }
     }
