6fac706047bff17128212e871bc3726766ae6f69
Travis Vela <jtravisvela@gmail.com>
2019-03-22 09:13:03 -0500

    add solutions to String exercise

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -5,5 +5,6 @@ public class StringExercise {
         System.out.println("Check \"this\" out! \"s inside of\" s!");
         System.out.println("In Windows, then main drive is usually C:\\");
         System.out.println("I can do backslashes \\, double backslashes \\\\,\nand the amazing triple backslash \\\\\\!");
+
     }
 }
