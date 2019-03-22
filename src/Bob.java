1d40d86f08c40b54a39fd336ecb54647c3c570ba
Travis Vela <jtravisvela@gmail.com>
2019-03-22 10:23:52 -0500

    add a way to exit conversation

--- src/Bob.java
+++ src/Bob.java
@@ -1,32 +1,36 @@
 import java.util.Scanner;
+import java.awt.event.KeyEvent;
 public class Bob {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Hello, meet Bob.");
+        System.out.println("Hello, meet Bob. He's all ears\ntype 'end' to leave the conversation.");
 
         while (true) {
             System.out.println("...");
+            System.out.println();
             String message = scanner.nextLine();
 
 
             if (message.endsWith("?")) {
                 message = "Sure";
                 System.out.println(message);
+                System.out.println();
             } else if (message.endsWith("!")) {
                 message = "Whoa, chill out!";
                 System.out.println(message);
+                System.out.println();
+            } else if (message.equals("end")) {
+                break;
             } else if (message.equals("") || message.indexOf("?") == -1 || message.indexOf("!") == -1) {
                 message = "Whatever.";
                 System.out.println(message);
+                System.out.println();
             }
 
-            //continue?
-//            System.out.println("Would you like to continue? [y/n]");
-//            if (scanner.nextLine().equals("n")) {
-//                break;
-//            }
+
+
         }
     }
 }
