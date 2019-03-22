6e2b321049cc7591df30bda5e39354eaa6f479ba
Travis Vela <jtravisvela@gmail.com>
2019-03-22 09:11:09 -0500

    add ControlFlow exercise

---
+++ src/StringExercise.java
@@ -0,0 +1,9 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        String message = "";
+        System.out.println("We don't need no education\nWe don't need no thought control");
+        System.out.println("Check \"this\" out! \"s inside of\" s!");
+        System.out.println("In Windows, then main drive is usually C:\\");
+        System.out.println("I can do backslashes \\, double backslashes \\\\,\nand the amazing triple backslash \\\\\\!");
+    }
+}
