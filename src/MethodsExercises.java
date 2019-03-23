f645d24006b17a0bb6cb7788a6e3f3aa2edf0cca
Travis Vela <jtravisvela@gmail.com>
2019-03-23 17:30:57 -0500

    add exerise 3 solution

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,14 +1,15 @@
 import java.util.Scanner;
 
 public class MethodsExercises {
-    Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("123 & 55 = " + addition(123, 55));
+        System.out.println("123 + 55 = " + addition(123, 55));
         System.out.println("123 - 55 = " + subtraction(123, 55));
         System.out.println("123 * 55 = " + multiplication(123, 55));
         System.out.println("123 / 55 = " + division(123, 55));
         System.out.println("123 % 55 = " + modulus(123, 55));
         getInteger(1, 10);
+        numberFactorial(1, 10);
     }
 
     public static int addition(int arg1, int arg2) {
@@ -33,13 +34,31 @@ public class MethodsExercises {
 
     public static int getInteger(int min, int max) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a number between 1 and 10: ");
+        System.out.print("Enter a number between 1 and 10: ");
         int userInput = scanner.nextInt();
         if (userInput >= min && userInput <= max) {
             return getInteger(min, max);
         } else {
+            System.out.printf("%d is not in range.%n", userInput);
             return userInput;
         }
+    }
+
+    public static long numberFactorial(int min, int max) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number between 1 and 10: ");
+        int input = scanner.nextInt();
+        long fact = 1;
+        String result = "";
+        if (input >= min && input <= max) {
+            for (int i = 1; i <= input; i++) {
+                fact *= i;
+                result += i + " x ";
+                System.out.printf("%d! = %s = %d%n", i, result.substring(0, result.length() - 2), fact);
+            }
+            return fact;
+        }
+        return fact;
     }
 
 }
