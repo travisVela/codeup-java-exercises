92f1d0aa96fcf89cd267b769e166a1f0200322ea
Travis Vela <jtravisvela@gmail.com>
2019-03-24 20:15:44 -0500

    add additional styling to game

--- src/HighLow.java
+++ src/HighLow.java
@@ -1,9 +1,13 @@
 import java.util.Scanner;
 public class HighLow {
     public static void main(String[] args) {
-        int randomNumber = (int) (Math.random() * 100) + 1;
-        int count = 1;
-        System.out.println(randomNumber);
+        highLow( (int) (Math.random() * 100) + 1);
+    }
+
+     public static void highLow(int randomNumber) {
+        randomNumber = (int) (Math.random() * 100) + 1;
+        int count = 10;
+//        System.out.println(randomNumber);
         Scanner scanner = new Scanner(System.in);
         System.out.println("================================");
         System.out.println();
@@ -11,7 +15,7 @@ public class HighLow {
         System.out.println();
         System.out.println("================================");
         System.out.println();
-        System.out.println("Choose a number between 1 and 100");
+        System.out.println("You get 10 guesses\nChoose a number between 1 and 100");
         while (true) {
             int guess = scanner.nextInt();
             if (guess == randomNumber) {
@@ -20,22 +24,44 @@ public class HighLow {
                 System.out.println("You guessed it!!");
                 System.out.println();
                 System.out.println("================");
-                break;
-            } else if (count == 5) {
+                System.out.print("Do you want to try again? [y/n] ");
+                if(scanner.next().equals("n")) {
+                    System.out.println("===================");
+                    System.out.println();
+                    System.out.println("Thanks for playing!!");
+                    System.out.println();
+                    System.out.println("===================");
+                    break;
+                } else {
+                    highLow(randomNumber);
+                }
+            } else if (count == 0) {
                 System.out.println("============");
                 System.out.println();
                 System.out.println("Game Over :(");
                 System.out.println();
                 System.out.println("============");
-                break;
+                System.out.print("Do you want to try again? [y/n] ");
+                if(scanner.next().equals("n")) {
+                    System.out.println("===================");
+                    System.out.println();
+                    System.out.println("Thanks for playing!");
+                    System.out.println();
+                    System.out.println("===================");
+                    break;
+                } else {
+                    highLow(randomNumber);
+                }
             } else if (guess < randomNumber) {
                 System.out.println("Higher!!");
-                System.out.println(count);
+
             } else if (guess > randomNumber) {
                 System.out.println("Lower!!");
-                System.out.println(count);
+
             }
-            count++;
+            count--;
+            System.out.println("(" + count + " guesses left)");
+
         }
     }
 }
